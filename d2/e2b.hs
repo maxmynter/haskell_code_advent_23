cfdb189f1df03b8bf3901be69a09b9af65f33131
Max Mynter <maxmynter@me.com>
2024-09-29 01:29:49 +0200

    Day2 b

--- d2/e2b.hs
+++ d2/e2b.hs
@@ -14,17 +14,14 @@ data Round = Round Red Blue Green
 
 data Game = Game ID [Round]
 
-isRoundValid :: Red -> Blue -> Green -> Round -> Bool
-isRoundValid maxR maxG maxB rnd@(Round r g b) =
-  r <= maxR && g <= maxG && b <= maxB
+extractMaxOccurrences :: Game -> (Red, Blue, Green)
+extractMaxOccurrences (Game _ rounds) = go (0, 0, 0) rounds
+  where
+    go rgbs [] = rgbs
+    go (maxR, maxG, maxB) ((Round r g b) : rs) = go (max r maxR, max maxG g, max maxB b) rs
 
-allRoundsValid :: Red -> Blue -> Green -> [Round] -> Bool
-allRoundsValid maxR maxG maxB rounds =
-  and [isRoundValid maxR maxG maxB round | round <- rounds]
-
-idIfValidElseZero :: Red -> Green -> Blue -> Game -> Int
-idIfValidElseZero maxR maxG maxB game@(Game id rounds) =
-  if allRoundsValid maxR maxG maxB rounds then id else 0
+power :: Game -> Int
+power gm = let (r, g, b) = extractMaxOccurrences gm in r * g * b
 
 parseColorStr :: String -> Round
 parseColorStr s = foldl go (Round 0 0 0) (splitOn "," s)
@@ -48,6 +45,6 @@ parseGame s =
 
 main :: IO ()
 main = do
-  content <- readFile "input.txt"
-  let result = sum [idIfValidElseZero 12 13 14 (parseGame l) | l <- lines content]
+  content <- readFile "d2/input.txt"
+  let result = sum [power (parseGame l) | l <- lines content]
   print result
